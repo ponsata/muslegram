ca76135734197f9189063c84aa30c2a388d3d7aa
ponsata <khr.04237@gmail.com>
2020-09-09 19:12:29 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
       get 'find_videos',     to: 'youtubes#results'
       get 'results',         to: 'youtubes#results'
     end
-    resources :messages, only:[:index, :new, :create, :show, :destroy]
+    
   end
   resources :users, only:[:show]
+  resources :messages, only:[:index, :new, :create, :show, :destroy]
+
 end
